2cc75c7c371d2309d597c2e61022baaeb111df43
Juan Vasquez <117683116+juanvasquez10@users.noreply.github.com>
2024-03-05 12:21:24 -0500

    Update app.py
    v2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<html><head><style>body { color: darkblue; background-color: lightgrey; }</style></head><body><h1>Hello, World!</h1></body></html>'
+    return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
